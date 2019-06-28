4fb2cea4686e5f9c8ebcaea597fa97079c26a69e
Dominic Canora <dominic.canora@disney.com>
2019-06-28 15:53:25 -0400

    Modify get members to reflect new team wording

--- teammanager/urls.py
+++ teammanager/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import checkin_api
 
 urls = [
-    path('api/getstudents', checkin_api.get_members, name="api_members"),
+    path('api/getmembers', checkin_api.get_members, name="api_members"),
     path('api/punch', checkin_api.punch, name="api_punch"),
 ]
