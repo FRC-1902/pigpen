b52353ead715d1e4a17749d6faa02c7a99eb1444
Dominic Canora <dominic.canora@disney.com>
2019-07-02 15:02:27 -0400

    User authentication

--- teammanager/views/auth.py
+++ teammanager/views/auth.py
@@ -1,6 +1,21 @@
+from django.contrib.auth import authenticate, login as dj_login, logout as dj_logout
+from django.http.response import HttpResponseBadRequest
+from django.shortcuts import redirect
+
+
 def login(request):
-    pass
+    if request.method == 'POST':
+        data = request.POST
+        if 'username' in data and 'password' in data:
+            user = authenticate(request, username=data['username'], password=data['password'])
+            if user is not None:
+                dj_login(request, user)
+
+            return redirect('man:index')
+
+    return HttpResponseBadRequest()
 
 
 def logout(request):
-    pass
+    dj_logout(request)
+    return redirect('man:index')
