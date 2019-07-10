96448924b8c6accaf67b32c9298f042e387b3917
Dominic Canora <dominic.canora@disney.com>
2019-07-10 13:47:21 -0400

    Leaderboard view for hours

--- teammanager/views/hours.py
+++ teammanager/views/hours.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.urls import reverse
 
 from ..models import Member
 
@@ -12,9 +11,10 @@ def hours(request):
 
 @login_required
 def hours_table(request):
+    sort_leader = bool(request.GET.get('sort', 'default') == "leader")
     members = list(Member.objects.order_by("-role", "first"))
 
-    head = ["Name", "Build", "Outreach"]
+    head = ["Name", "Total", "Build", "Outreach"]
     students = []
     adults = []
 
@@ -23,15 +23,21 @@ def hours_table(request):
         if member.role == 'stu':
             students.append(tuple([
                 member.short_name(),
+                str(hours.get("total", "0")).split(':')[0],
                 str(hours.get("build", "0")).split(':')[0],
                 str(hours.get("out", 0)).split(':')[0]
             ]))
         else:
             adults.append(tuple([
                 member.short_name(),
+                str(hours.get("total", "0")).split(':')[0],
                 str(hours.get("build", "0")).split(':')[0],
                 str(hours.get("out", 0)).split(':')[0]
             ]))
+
+    if sort_leader:
+        students = sorted(students, key=lambda x: int(x[1]))
+        adults = sorted(students, key=lambda x: int(x[1]))
 
     return render(request, 'teammanager/partial/hours_table.html', {
         "head": head,
