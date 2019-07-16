0d082a9cb023d7bcda002177b621d4f8ad055d4c
Dominic Canora <dominic.canora@disney.com>
2019-07-16 12:12:12 -0400

    Set correct environment key

--- pigpen/settings.py
+++ pigpen/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-is_prod = bool(os.getenv("prod", True))
+is_prod = bool(os.getenv("PROD", True))
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
