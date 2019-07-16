7ab51af57f43a40311e6eb91394430f88259b51e
Ryan <ryan.shavell@gmail.com>
2019-07-16 17:33:01 -0400

    Better display for when nobody is checked in

--- teammanager/views/index.py
+++ teammanager/views/index.py
@@ -37,6 +37,7 @@ def location(request):
             "name": member.short_name(),
             "position": member.role,
             "isIn": bool(member in members_in),
-            "avatar": member.get_avatar()
+            "avatar": member.get_avatar(),
+            "any_checkins": len(members_in) > 0
         })
     return render(request, 'teammanager/location.html', {"members": out})
