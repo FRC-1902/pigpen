82925c5e25517acceb60d933518536818a4ebacb
Dominic Canora <dominic.canora@disney.com>
2019-07-16 14:38:58 -0400

    Location: update sort order

--- teammanager/views/index.py
+++ teammanager/views/index.py
@@ -4,7 +4,6 @@ from ..models import Member, Punch
 
 
 def index(request):
-
     members = list(Member.objects.order_by("-role", "first"))
 
     students = []
@@ -25,13 +24,13 @@ def index(request):
                     member.get_avatar()
                 ]))
 
-
     students = sorted(students, key=lambda x: float(x[1]), reverse=True)
 
     return render(request, 'teammanager/index.html', {"students": students[0:5]})
 
+
 def location(request):
-    members = Member.objects.all().order_by("first")
+    members = Member.objects.all().order_by("first", "last")
     out = []
     members_in = [x.member for x in Punch.objects.filter(end__isnull=True)]
 
