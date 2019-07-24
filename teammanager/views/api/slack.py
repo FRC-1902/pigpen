0c4ca18c43ea2997c0cce8f048bd404493bad41f
Ryan <ryan.shavell@gmail.com>
2019-07-24 11:28:47 -0400

    expand /outreach response

--- teammanager/views/api/slack.py
+++ teammanager/views/api/slack.py
@@ -6,4 +6,36 @@ def action(request):
 
 
 def outreach(request):
-    return HttpResponse(status=200)
+    return JsonResponse([
+    {
+        "type": "section",
+        "text": {
+            "type": "plain_text",
+            "text": "Welcome to the Outreach Manager! What would you like to post?",
+            "emoji": True
+        }
+    },
+    {
+        "type": "actions",
+        "elements": [
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Outreach Signup",
+                    "emoji": True
+                },
+                "value": "signup"
+            },
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Outreach Check-In",
+                    "emoji": True
+                },
+                "value": "checkin"
+            }
+        ]
+    }
+])
