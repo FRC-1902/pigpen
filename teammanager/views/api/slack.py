7383b0706a0ff303a43e6d9aa24e374abc29adcb
Ryan <ryan.shavell@gmail.com>
2019-07-24 11:40:29 -0400

    Test fixing JSONresponse

--- teammanager/views/api/slack.py
+++ teammanager/views/api/slack.py
@@ -9,4 +9,38 @@ def action(request):
 
 @csrf_exempt
 def outreach(request):
-    return HttpResponse("Outreach! Yay!")
+    return JsonResponse({
+        [
+            {
+                "type": "section",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Welcome to the Outreach Manager! What would you like to post?",
+                    "emoji": True
+                }
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Outreach Signup",
+                            "emoji": True
+                        },
+                        "value": "signup"
+                    },
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Outreach Check-In",
+                            "emoji": True
+                        },
+                        "value": "checkin"
+                    }
+                ]
+            }
+        ]
+    })
