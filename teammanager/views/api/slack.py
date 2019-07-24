6dcde432b7fa6412c958be1a2a8d1114f9e79822
Ryan <ryan.shavell@gmail.com>
2019-07-24 11:44:56 -0400

    use test object

--- teammanager/views/api/slack.py
+++ teammanager/views/api/slack.py
@@ -9,38 +9,11 @@ def action(request):
 
 @csrf_exempt
 def outreach(request):
-    return JsonResponse(
-        [
-            {
-                "type": "section",
-                "text": {
-                    "type": "plain_text",
-                    "text": "Welcome to the Outreach Manager! What would you like to post?",
-                    "emoji": True
-                }
-            },
-            {
-                "type": "actions",
-                "elements": [
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Outreach Signup",
-                            "emoji": True
-                        },
-                        "value": "signup"
-                    },
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Outreach Check-In",
-                            "emoji": True
-                        },
-                        "value": "checkin"
-                    }
-                ]
-            }
-        ]
-    , safe=False)
+    return JsonResponse({
+    "text": "It's 80 degrees right now.",
+    "attachments": [
+        {
+            "text":"Partly cloudy today and tomorrow"
+        }
+    ]
+})
