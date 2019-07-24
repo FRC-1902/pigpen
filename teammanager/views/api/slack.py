93256b549f5086bb501a527ec17ecb5a05ac9680
Ryan <ryan.shavell@gmail.com>
2019-07-24 19:41:50 -0400

    fix meeting id

--- teammanager/views/api/slack.py
+++ teammanager/views/api/slack.py
@@ -276,7 +276,7 @@ def outreach_signup_blocks(meeting):
             "text": {
                 "type": "mrkdwn",
                 "text": "Sign-up for outreach event *{}* opened! Be sure you can attend before you click on the button."
-                        "Sign-up list: {}".format(reverse("man:meeting", args={"id": meeting.id}), meeting)
+                        "Sign-up list: {}".format(reverse("man:meeting", kwargs={"id": meeting.id}), meeting)
             }
         },
         {
