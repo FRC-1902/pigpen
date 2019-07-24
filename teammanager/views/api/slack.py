1a9429fa0392608ef3f8cdcac38a636b2521cb9f
Ryan <ryan.shavell@gmail.com>
2019-07-24 11:33:07 -0400

    test plaintext response

--- teammanager/views/api/slack.py
+++ teammanager/views/api/slack.py
@@ -9,36 +9,4 @@ def action(request):
 
 @csrf_exempt
 def outreach(request):
-    return JsonResponse([
-    {
-        "type": "section",
-        "text": {
-            "type": "plain_text",
-            "text": "Welcome to the Outreach Manager! What would you like to post?",
-            "emoji": True
-        }
-    },
-    {
-        "type": "actions",
-        "elements": [
-            {
-                "type": "button",
-                "text": {
-                    "type": "plain_text",
-                    "text": "Outreach Signup",
-                    "emoji": True
-                },
-                "value": "signup"
-            },
-            {
-                "type": "button",
-                "text": {
-                    "type": "plain_text",
-                    "text": "Outreach Check-In",
-                    "emoji": True
-                },
-                "value": "checkin"
-            }
-        ]
-    }
-])
+    return HttpResponse("Outreach! Yay!")
